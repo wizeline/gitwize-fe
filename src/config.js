f2ac99b723846fde55e9eb2295b9c3ff1795cfa9
Hieu Nguyen <hieu.nguyen@wizeline.com>
2020-05-05 19:22:50 +0700

    remove credential info

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = process.env.CLIENT_ID || '0oaawcopl4ebL8ONR4x6'
+const CLIENT_ID = process.env.CLIENT_ID || 'clientID'
 const ISSUER = process.env.ISSUER || 'https://dev-311502.okta.com/oauth2/default'
 const OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false
 
