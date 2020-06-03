ae7b5d4cdad23da1f930334332fb5c25890a5043
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-06-03 09:51:51 +0700

    Implement global navigation based on wireframe

--- src/contexts/MainLayoutContext.js
+++ src/contexts/MainLayoutContext.js
@@ -3,6 +3,7 @@ import React from 'react'
 const initialValue = {
   handleChangeRepositoryId: (repositoryId) => {},
   handleDisplaySubMenu: (isDisplayDashBoard) => {},
+  handleChangeRepositoryName: (repositoryName) => {}
 }
 const MainLayoutContex = React.createContext(initialValue)
 
