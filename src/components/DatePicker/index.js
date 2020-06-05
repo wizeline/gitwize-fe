bb3e01cb125712721828fcfd245291a15e99268d
huynguyen <huy.nguyen@wizeline.com>
2020-06-05 15:42:12 +0700

    Component Datepicker to choose in a range

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -2,12 +2,37 @@ import React, { useState } from 'react';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import Paper from '@material-ui/core/Paper'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    marginTop: theme.spacing(2),
+    paddingRight: theme.spacing(5),
+    width: '100%',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  datePicker: {
+    position: 'absolute',
+    zIndex: 99,
+    background: 'rgba(0, 0, 0, 0.85)',
+    color: '#ffff',
+    marginTop: '10px'
+  }
+}))
 
 export default function DatePicker() {
   const [dateRange, setDateRange] = useState({
     from: undefined,
     to: undefined
   })
+  const [openDatePicker, setOpenDatePicker] = useState(false)
+
+  const styles = useStyles()
   
   const getInitialState = () => {
     return {
@@ -18,6 +43,8 @@ export default function DatePicker() {
 
   const handleDayClick = (day) => {
     const range = DateUtils.addDayToRange(day, dateRange)
+    if(range.to !== undefined)
+      setOpenDatePicker(false)
     setDateRange(range);
   }
 
@@ -25,48 +52,70 @@ export default function DatePicker() {
     setDateRange(getInitialState)
   }
 
+  const toggleDatePicker = () => {
+    setOpenDatePicker(!openDatePicker)
+  }
+
     const { from, to } = dateRange
     const modifiers = { start: from, end: to };
+
+    const selectedDateRange = () => {
+      if(!from && !to)
+        return 'Please select the first day'
+      if (from && !to)
+        return 'Please select the last day'
+      if(from && to)
+        return  `Selected from ${from.toLocaleDateString()} - ${to.toLocaleDateString()}`
+    }
+
     return (
-      <div className="RangeExample">
-        <p>
-          {!from && !to && 'Please select the first day.'}
-          {from && !to && 'Please select the last day.'}
-          {from &&
-            to &&
-            `Selected from ${from.toLocaleDateString()} -
-                ${to.toLocaleDateString()}`}{' '}
-          {from && to && (
-            <button className="link" onClick={handleResetClick}>
-              Reset
-            </button>
-          )}
-        </p>
+      <FormControl className={styles.formControl}>
+        <TextField
+          id='standard-helperText'
+          label={'Date range'}
+          value={selectedDateRange()}
+          InputProps={{
+            readOnly: true,
+          }}
+          onClick={toggleDatePicker}
+        />
+
+      <div className='RangeExample'>
+        <Paper style={{display: `${openDatePicker ? "block" : "none"}` }} 
+          className={styles.datePicker}>
         <DayPicker
-          className="Selectable"
+          className='Selectable'
           numberOfMonths={2}
           selectedDays={[from, { from, to }]}
           modifiers={modifiers}
           onDayClick={handleDayClick}
           show={false}
         />
+        </Paper>
           <style>{`
   .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
-    background-color: #f0f8ff !important;
-    color: #4a90e2;
+    opacity: 0.9;
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9)), #000000;
   }
   .Selectable .DayPicker-Day {
     border-radius: 0 !important;
   }
   .Selectable .DayPicker-Day--start {
-    border-top-left-radius: 50% !important;
-    border-bottom-left-radius: 50% !important;
+    border-radius: 100% !important;
   }
   .Selectable .DayPicker-Day--end {
-    border-top-right-radius: 50% !important;
-    border-bottom-right-radius: 50% !important;
+    border-radius: 100% !important;
   }
+  .Selectable .DayPicker-Month {
+    width: 100%
+  }
+
+  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--disabled):not(.DayPicker-Day--outside) {
+    background: #EC5D5C;
+  }
+
 `}</style>
       </div>
+      </FormControl>
     );
 }
