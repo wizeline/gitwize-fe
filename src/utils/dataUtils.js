fc94caa1f745420186ce4fbd8d7746f45b7fcee5
huynguyen <huy.nguyen@wizeline.com>
2020-06-08 09:35:13 +0700

    Update unit tests

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,19 +1,3 @@
-export const filterObjectByKey = (objects, allowed = []) => {
-  const filteredData = []
-  objects.forEach((row) => {
-    const filteredRow = Object.keys(row)
-      .filter((key) => allowed.includes(key))
-      .reduce((obj, key) => {
-        obj[key] = row[key]
-        return obj
-      }, {})
-
-    filteredData.push(filteredRow)
-  })
-
-  return filteredData
-}
-
 export const createReversedArray = (array) => {
   const reversedArray = array.reduce((acc, b) => [b, ...acc], [])
 
