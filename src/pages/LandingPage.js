3b50c19d58be7404c29eaa8ceba1bac3f4b15654
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-06-18 09:33:02 +0700

    fix build error

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,7 +3,6 @@ import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import { Button } from '@material-ui/core'
 import styled from 'styled-components'
-import Container from '@material-ui/core/Container'
 
 import Iframe from '../components/Iframe'
 
