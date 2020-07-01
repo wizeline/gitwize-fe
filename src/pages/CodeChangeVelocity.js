b3cd8652a4da3b2bddb20f9529851bb993b93a67
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-07-01 13:40:50 +0700

    Implement chart to get data from json file

--- src/pages/CodeChangeVelocity.js
+++ src/pages/CodeChangeVelocity.js
@@ -37,73 +37,74 @@ const useStyles = makeStyles(() => ({
 }))
 
 const buildChartOptionsBasedOnMaxValue = (chartData) => {
-  const maxValue = chartData.reduce((a, b) => {
-    return Math.max(a, b);
-  })
-  return  {
-    scales: {
-      xAxes: [
-        {
-          display: true,
-          gridLines: {
-            display: false
-          },
-          stacked: false,
-          ticks: {
-            fontColor: "#C4C4C4",
-            fontSize: 10,
-            autoSkip: true,
-            autoSkipPadding: 30
+  if(chartData) {
+    const chartValue = Object.values(chartData)
+    const maxValue = chartValue.reduce((a, b) => {
+      return Math.max(a, b);
+    })
+    return  {
+      scales: {
+        xAxes: [
+          {
+            display: true,
+            gridLines: {
+              display: false
+            },
+            stacked: false,
+            ticks: {
+              fontColor: "#C4C4C4",
+              fontSize: 10,
+              autoSkip: true,
+              autoSkipPadding: 30
+            }
           }
+        ],
+        yAxes: [
+          {
+            type: 'linear',
+            display: false,
+            position: 'left',
+            id: 'y-axis-1',
+            gridLines: {
+              display: false
+            },
+            labels: {
+              show: false
+            },
+            stacked: false,
+            ticks: {
+              fontColor: "#C4C4C4",
+              fontSize: 10,
+              beginAtZero: true,
+              min: 0,
+              max: (maxValue + moreValueToAddYAxis),
+              precision: 0,
+              suggestedMax: 5
+            }
+          }
+        ]
+      },
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        datalabels: {
+            anchor: 'end',
+            align: 'right',
+            offset: 20,
+            font: {
+              size: 14
+            }
         }
-      ],
-      yAxes: [
-        {
-          type: 'linear',
-          display: false,
-          position: 'left',
-          id: 'y-axis-1',
-          gridLines: {
-            display: false
-          },
-          labels: {
-            show: false
-          },
-          stacked: false,
-          ticks: {
-            fontColor: "#C4C4C4",
-            fontSize: 10,
-            beginAtZero: true,
-            min: 0,
-            max: (maxValue + moreValueToAddYAxis),
-            precision: 0,
-            suggestedMax: 5
-          }
-        }
-      ]
-    },
-    tooltips: {
-      enabled: false
-    },
-    plugins: {
-      datalabels: {
-          anchor: 'end',
-          align: 'right',
-          offset: 20,
-          font: {
-            size: 14
-          }
-      }
-    },
-    maintainAspectRatio: false,
+      },
+      maintainAspectRatio: false,
+    }
   }
 }
 
-const transformChartData = (response, chartBars, fieldName) => {
-  const chartBar = chartBars[0]
-  const data = response[fieldName]
+const transformChartData = (data, chartBar) => {
   if(data) {
-    const labels = Object.keys(chartData);
+    const labels = Object.keys(data);
     const chartData = []
     labels.forEach(item => {
       chartData.push(data[item])
@@ -142,114 +143,12 @@ const transformChartData = (response, chartBars, fieldName) => {
   }
 }
 
-const chartBarNumberOfCommits = [{name:'Number Of commit', color: '#62C8BA'}]
-const chartBarNetChanges = [{name:'Net Changes', color: '#9F55E2'}]
-const chartBarNewCode = [{name:'New Code Percentage', color: '#EC5D5C'}]
+const chartBars = [
+                    {name:'Number Of commit', color: '#62C8BA', fieldName: 'commits', chartId: 'chartLegendId-1'},
+                    {name:'New Code Percentage', color: '#EC5D5C', fieldName: 'newCodeChanges', chartId: 'chartLegendId-2'},
+                    {name:'Net Changes', color: '#9F55E2',fieldName: 'netChanges', chartId: 'chartLegendId-3'}, 
+                  ]
 const chartLines = []
-const chartDataNumberOfCommit = {
-  labels: [
-  'March',
-  'April',
-  'May'
-  ],
-  datasets: [
-    {
-      label: "Number Of commit",
-      backgroundColor: "#62C8BA",
-      borderColor: "#62C8BA",
-      hoverBackgroundColor: "#62C8BA",
-      borderWidth: 1,
-      data: [121, 221, 32],
-      barPercentage: 0.7,
-      categoryPercentage:  0.5,
-      datalabels: {
-        color: '#62C8BA',
-        font: {
-          weight: 'bold'
-        },
-        formatter: function (value, context) {
-          const {dataIndex, dataset} = context
-          if(dataIndex === 0) {
-            return ''
-          } else {
-            value = Math.round(((value - dataset.data[dataIndex - 1]) / dataset.data[dataIndex - 1]) * 100)
-            return value + '%'
-          }
-        }
-      }
-    },
-  ]
-}
-
-const chartDataNewCodePercentage = {
-  labels: [
-  'March',
-  'April',
-  'May'
-  ],
-  datasets: [
-    {
-      label: "New Code Percentage",
-      backgroundColor: "#EC5D5C",
-      borderColor: "#EC5D5C",
-      hoverBackgroundColor: "#EC5D5C",
-      borderWidth: 1,
-      data: [111, 22, 33],
-      barPercentage: 0.5,
-      categoryPercentage:  0.7,
-      datalabels: {
-        color: '#EC5D5C',
-        font: {
-          weight: 'bold'
-        },
-        formatter: function (value, context) {
-          const {dataIndex, dataset} = context
-          if(dataIndex === 0) {
-            return ''
-          } else {
-            value = Math.round(((value - dataset.data[dataIndex - 1]) / dataset.data[dataIndex - 1]) * 100)
-            return value + '%'
-          }
-        }
-      }
-    }
-  ]
-}
-
-const chartDataNetChange = {
-  labels: [
-  'March',
-  'April',
-  'May'
-  ],
-  datasets: [
-    {
-      label: "Net Changes",
-      backgroundColor: "#9F55E2",
-      borderColor: "#9F55E2",
-      hoverBackgroundColor: "#9F55E2",
-      borderWidth: 1,
-      data: [12, 23, 35],
-      barPercentage: 0.5,
-      categoryPercentage:  0.7,
-      datalabels: {
-        color: '#9F55E2',
-        font: {
-          weight: 'bold'
-        },
-        formatter: function (value, context) {
-          const {dataIndex, dataset} = context
-          if(dataIndex === 0) {
-            return ''
-          } else {
-            value = Math.round(((value - dataset.data[dataIndex - 1]) / dataset.data[dataIndex - 1]) * 100)
-            return value + '%'
-          }
-        }
-      }
-    },
-  ]
-}
 
 const calculatedateRange = () => {
   const currentDate = getCurrentDate()
@@ -264,18 +163,18 @@ const calculatedateRange = () => {
 
 function CodeChangeVelocity(props) {
   const {information} = props;
-  const [chartData, setChartData] = useState()
+  const [responseData, setResponseData] = useState({})
   const { authState } = useOktaAuth()
   const mainLayout = useRef(useContext(MainLayoutContex))
   const { id } = props.match.params
   const classes = useStyles();
-  // const dateRange = calculatedateRange()
 
   useEffect(() => {
     apiClient.setAccessToken(authState.accessToken)
     mainLayout.current.handleChangeRepositoryId(id)
     const dateRange  = calculatedateRange()
     apiClient.codeChangeVelocity.getCodeChangeVelocityStats(id, dateRange).then((data) => {
+      setResponseData(data)
       console.log(data, transformChartData(data, 'commits'))
     })
   }, [id, authState.accessToken])
@@ -286,15 +185,11 @@ function CodeChangeVelocity(props) {
       <Grid container className={classes.root}>
         <Grid className={classes.gridItem} style={{justifyContent: 'flex-end'}} item xs={12}>
             <Grid container className={classes.root}>
-              <Grid className={classes.gridItem} item xs={4}>
-                  <Chart data={chartDataNumberOfCommit} chartOptions={buildChartOptionsBasedOnMaxValue(chartDataNumberOfCommit.datasets[0].data)} chartBars={chartBars} chartLines={chartLines} chartLegendId={'chartLegendId-1'}/>
-              </Grid>
-              <Grid className={classes.gridItem} item xs={4}>
-                  <Chart data={chartDataNewCodePercentage} chartOptions={buildChartOptionsBasedOnMaxValue(chartDataNewCodePercentage.datasets[0].data)} chartBars={chartBars} chartLines={chartLines} chartLegendId={'chartLegendId-2'}/>
-              </Grid>
-              <Grid className={classes.gridItem} item xs={4}>
-                  <Chart data={chartDataNetChange} chartOptions={buildChartOptionsBasedOnMaxValue(chartDataNetChange.datasets[0].data)} chartBars={chartBars} chartLines={chartLines} chartLegendId={'chartLegendId-3'}/>
-              </Grid>
+              {chartBars.map(chartItem => {
+                return (<Grid key={chartItem.chartId} className={classes.gridItem} item xs={4}>
+                          <Chart data={transformChartData(responseData[chartItem.fieldName], chartItem)} chartOptions={buildChartOptionsBasedOnMaxValue(responseData[chartItem.fieldName])} chartBars={chartBars} chartLines={chartLines} chartLegendId={chartItem.chartId}/>
+                        </Grid>)
+              })}
             </Grid>
         </Grid>
       </Grid>
