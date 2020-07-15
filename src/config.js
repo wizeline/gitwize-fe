017c3f5881844c207bbeb37a13acb5483d8bce22
huynguyen <huy.nguyen@wizeline.com>
2020-07-15 11:21:28 +0700

    Add configuration to renew token

--- src/config.js
+++ src/config.js
@@ -13,5 +13,9 @@ export default {
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: REACT_APP_OKTA_DISABLE_HTTPS_CHECK,
+    tokenManager: {
+      storage: 'sessionStorage',
+      autoRenew: true
+    }
   },
 }
