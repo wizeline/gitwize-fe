ccaefed32c26264f18a477102ea134360e78d2c0
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-07-24 14:05:58 +0700

    Implement new chart quarterly trends

--- src/views/DataStats.js
+++ src/views/DataStats.js
@@ -7,6 +7,7 @@ import TableData from '../components/TableData'
 import Chart from '../components/Chart'
 import useToggle from '../hooks/useToggle'
 import BranchFilter from '../components/BranchFilter'
+import {getChartOptions} from '../utils/chartUtils'
 
 const showDate = ['Last 7 Days', 'Last 14 Days', 'Last 21 Days', 'Last 30 Days', 'Custom']
 const useStyles = makeStyles(() => ({
@@ -60,7 +61,7 @@ function DataStats(props) {
             </Grid>
           </Grid>
           {!isDisplayChart && <TableData tableData={tableData} tableColumn={tableColumn} isDisplaySearch={isDisplaySearch} customComponent={tableCustomComponent}/>}
-          {isDisplayChart && <Chart data={chartData} chartOptions={chartOptions} chartBars={chartBars} chartLines={chartLines}/>}
+          {isDisplayChart && <Chart data={chartData} chartOptions={getChartOptions(chartOptions, chartLines)} chartBars={chartBars} chartLines={chartLines}/>}
         </>
       )
 }
