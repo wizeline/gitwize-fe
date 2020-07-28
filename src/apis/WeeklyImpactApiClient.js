e8468cf6d8fe541ad2f32d0b9f14afd512021455
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-07-28 15:07:28 +0700

    Integrate with real backend

--- src/apis/WeeklyImpactApiClient.js
+++ src/apis/WeeklyImpactApiClient.js
@@ -1,10 +1,9 @@
 import { BaseApiClient } from './base'
-import response from '../mockData/WeeklyImpactData.json'
-// const GET_WEEKLY_IMPACT_STATS_PATH = (repoId) => `/repositories/${repoId}/impact/weekly`
+
+const GET_WEEKLY_IMPACT_STATS_PATH = (repoId) => `/repositories/${repoId}/impact/weekly`
 
 export default class WeeklyImpactApiClient extends BaseApiClient {
   getWeeklyImpactStats(repoId) {
-    return Promise.resolve(response)
-    // return this.httpClient.get(GET_WEEKLY_IMPACT_STATS_PATH(repoId))
+    return this.httpClient.get(GET_WEEKLY_IMPACT_STATS_PATH(repoId))
   }
 }
