4bf34da0aa3659c10f7868772ff01e0fda909d0e
huynguyen <huy.nguyen@wizeline.com>
2020-08-07 11:01:30 +0700

    fix:okta-token

--- src/views/AppRouteAuthen.js
+++ src/views/AppRouteAuthen.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { useOktaAuth } from '@okta/okta-react'
+import { useOktaAuth, LoginCallback } from '@okta/okta-react'
 
 import NotFoundError404 from '../pages/NotFoundError404'
 import Home from '../pages/Home'
 import LandingPage from '../pages/LandingPage'
 import Loading from '../components/Loading'
-import ImplicitCallback from '../components/ImplicitCallback'
 
 export default function AppRouteAuthen() {
   const { authState } = useOktaAuth()
@@ -18,7 +17,7 @@ export default function AppRouteAuthen() {
   return (
     <Router>
       <Switch>
-        <Route path="/implicit/callback" component={ImplicitCallback} />
+        <Route path="/implicit/callback" component={LoginCallback} />
         {!authState.isAuthenticated && <Route exact path="/" component={LandingPage} />}
         {authState.isAuthenticated && <Route path="/" component={Home} />}
         <Route component={NotFoundError404} />
