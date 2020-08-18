279931ae85ccde6dba0aaf2337e20ef340e27aa7
Nhan Nguyen <nhan.nguyen@wizeline.com>
2020-08-18 13:35:04 +0700

    hide the value if previous is equal current

--- src/pages/WeeklyImpact.js
+++ src/pages/WeeklyImpact.js
@@ -376,6 +376,10 @@ const buildImpactScoreReasonSession = (response) => {
           currentValue = response[item.fieldName[0]].currentPeriod + response[item.fieldName[1]].currentPeriod
         }
 
+        if(previousValue === currentValue) {
+          return ''
+        }
+
         if (item.name === 'old codes' || item.name === 'new codes') {
           previousValue = `${previousValue.toFixed(2)}%`
           currentValue = `${currentValue.toFixed(2)}%`
